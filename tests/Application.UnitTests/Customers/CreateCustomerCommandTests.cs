d7c32a65e1f145bcaaf651db7082d54216395c36
Dương Công Lâm <151899481+csd-lamduong@users.noreply.github.com>
2024-06-28 11:47:41 +0700

    test: Add create customer test cases

--- tests/Application.UnitTests/Customers/CreateCustomerCommandTests.cs
+++ tests/Application.UnitTests/Customers/CreateCustomerCommandTests.cs
@@ -1,43 +1,75 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Moq;
-using TestInitProject.Application.Common.Interfaces;
+using TestInitProject.Application;
 using TestInitProject.Application.Customers.Commands.CreateCustomer;
+using TestInitProject.Domain.Entities;
+using TestInitProject.Domain.Events;
 
 namespace Application.UnitTests;
 
 
+[TestFixture]
 public class CreateCustomerCommandTests
 {
-    // private static CreateCustomerCommand Command = new();
-    // private CreateCustomerCommandHandler _handler;
-    // private Mock<DbSet<Customer>> _mockSet;
-    // private Mock<IApplicationDbContext> _mockDbContext;
+    private CreateCustomerCommandHandler _handler;
+    private Mock<IUnitOfWork> _mockUnitOfWork;
+    private Mock<ICustomerRepository> _mockCustomerRepository;
 
-    // [SetUp]
-    // public void SetUp()
-    // {
-    //     _mockSet = new Mock<DbSet<Customer>>();
-    //     _mockDbContext = new Mock<IApplicationDbContext>();
-    //     _handler = new CreateCustomerCommandHandler(_mockDbContext.Object);
-    // }
 
-    // [Test]
-    // public async Task CreateCustomer_Should_Add_And_Save_Customer()
-    // {
-    //     // Arrange
-    //     Command.Name = "test1";
-    //     Command.Email = "test1@mailinator.com";
+    [SetUp]
+    public void SetUp()
+    {
+        _mockUnitOfWork = new Mock<IUnitOfWork>();
+        _mockCustomerRepository = new Mock<ICustomerRepository>();
+        _handler = new CreateCustomerCommandHandler(_mockUnitOfWork.Object, _mockCustomerRepository.Object);
+    }
 
-    //     _mockSet.Setup(m => m.Add(It.Is<Customer>(c => c.Name == Command.Name && c.Email == Command.Email)));
-    //     _mockDbContext.Setup(m => m.Customers).Returns(_mockSet.Object);
-    //     _mockDbContext.Setup(m => m.SaveChangesAsync(It.IsAny<CancellationToken>())).Returns(Task.FromResult(1));
+    [Test]
+    public async Task CreateCustomer_Should_Add_And_Save_Customer()
+    {
 
-    //     // Act
-    //     int result = await _handler.Handle(Command, default);
+        // Arrange
+        CreateCustomerCommand command = new()
+        {
+            Name = "test1",
+            Email = "test1@mailinator.com"
+        };
+ 
+        Customer entity = new Customer(Guid.NewGuid()).Create(command.Name, command.Email);
 
-    //     // Assert
-    //     _mockSet.Verify(m => m.Add(It.IsAny<Customer>()), Times.Once);
-    //     _mockDbContext.Verify(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
-    //     Assert.That(result, Is.EqualTo(1));
-    // }
+        _mockCustomerRepository.Setup(m => m.Add(entity));
+        _mockUnitOfWork.Setup(m => m.SaveChangesAsync(default));   
+
+        // Act
+        int result = await _handler.Handle(command, default);
+
+        // Assert
+        _mockCustomerRepository.Verify(m => m.Add(It.IsAny<Customer>()), Times.Once);
+        _mockUnitOfWork.Verify(m => m.SaveChangesAsync(default), Times.Once);
+        Assert.That(result, Is.EqualTo(1));
+    }
+
+    [Test]
+    public async Task CreateCustomer_Should_Create_One_Domain_Event()
+    {
+        // Arrange
+        CreateCustomerCommand command = new()
+        {
+            Name = "test1",
+            Email = "test1@mailinator.com"
+        };
+ 
+        Customer entity = new Customer(Guid.NewGuid()).Create(command.Name, command.Email);
+
+        _mockCustomerRepository.Setup(m => m.Add(entity));
+        _mockUnitOfWork.Setup(m => m.SaveChangesAsync(default));   
+
+        // Act
+        int result = await _handler.Handle(command, default);
+
+        // Assert
+        _mockCustomerRepository.Verify(m => m.Add(It.Is<Customer>(c => c.Name == "test1" && c.Email == "test1@mailinator.com")));
+        _mockCustomerRepository.Verify(m => m.Add(It.Is<Customer>(c => c.DomainEvents.Count == 1 && c.DomainEvents.FirstOrDefault() is CustomerCreatedEvent)), Times.Once);
+        Assert.That(result, Is.EqualTo(1));
+    }
 }
